7155d9365ce5204d281954a3e01eb684b5de285a
Dimas <ardhid667@gmail.com>
2026-07-12 01:20:06 +0700

    Update konfigurasi database menggunakan environment variables

--- backend/config/db.go
+++ backend/config/db.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"strings"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -13,27 +12,32 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("Error loading .env file")
-	}
-
+	// Mengambil data dari Variables Railway
+	// Pastikan nama variabel di Railway (DB_USER, DB_PASS, dll) sudah diisi
 	dbUser := os.Getenv("DB_USER")
 	dbPass := os.Getenv("DB_PASS")
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
 
+	// Pengecekan agar aplikasi memberi tahu jika ada variabel yang lupa diisi di Railway
+	if dbHost == "" || dbUser == "" {
+		panic("Error: Variabel database belum lengkap di Settings -> Variables Railway!")
+	}
+
+	// DSN untuk membuat database jika belum ada
 	serverDSN := fmt.Sprintf("%s:%s@tcp(%s:%s)/?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbPort)
 	serverDB, err := gorm.Open(mysql.Open(serverDSN), &gorm.Config{})
 	if err != nil {
 		panic(fmt.Sprintf("Gagal menyambung ke server database: %v", err))
 	}
 
+	// Membuat database jika belum ada
 	if err := serverDB.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS `%s` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci", sanitizeIdentifier(dbName))).Error; err != nil {
 		panic(fmt.Sprintf("Gagal membuat database: %v", err))
 	}
 
+	// DSN untuk koneksi utama ke database yang sudah dibuat/ada
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbPort, dbName)
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -42,6 +46,7 @@ func ConnectDatabase() {
 	}
 
 	DB = database
+	fmt.Println("Database terhubung dengan sukses!")
 }
 
 func sanitizeIdentifier(value string) string {
